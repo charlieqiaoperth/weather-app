0370872f3d80fbf41c905bb7335f413dce7106f1
silverwind <charlieqiaoperth@gmail.com>
2019-05-23 17:29:55 +0800

    to doc files for deploy

---
+++ doc/precache-manifest.d077e1bc3f3f49995e944ddb2bbcfca5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd0e84d446742c2bcb9e7cd46a159a7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7da890751f5cb9dce830",
+    "url": "/static/css/main.e03c4f8c.chunk.css"
+  },
+  {
+    "revision": "e1b0456e3d361b9cd0e1",
+    "url": "/static/js/2.a10c8c5b.chunk.js"
+  },
+  {
+    "revision": "7da890751f5cb9dce830",
+    "url": "/static/js/main.a4cfb53f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
