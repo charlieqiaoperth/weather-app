b1d8f246414c066ff1cd1e54a816ee1d8289be12
silverwind <charlieqiaoperth@gmail.com>
2019-05-23 21:40:13 +0800

    Updates

---
+++ precache-manifest.6677638066cf604e3a29d57055947341.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c1cc0d7508a15951902002bb046dacd",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "53a29a27774221bc339c",
+    "url": "/weather-app/static/css/main.e03c4f8c.chunk.css"
+  },
+  {
+    "revision": "e1b0456e3d361b9cd0e1",
+    "url": "/weather-app/static/js/2.a10c8c5b.chunk.js"
+  },
+  {
+    "revision": "53a29a27774221bc339c",
+    "url": "/weather-app/static/js/main.fa0e153c.chunk.js"
+  },
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  }
+]);
